ec1cc1f81bb1fbf561fbf22c196952aeb73213e3
Kushagra614 <kushagravardhan619@gmail.com>
2025-12-10 22:17:44 +0530

    Building...

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 enum class OrderType
@@ -27,46 +28,90 @@ using LevelInfos = vector<LevelInfo>;
 
 class OrderbookLevelInfos
 {
-    public:
-    OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
+public:
+    OrderbookLevelInfos(const LevelInfos &bids, const LevelInfos &asks)
     {
         bids_ = bids;
         asks_ = asks;
     }
 
-    //Adding some public APIs
-    const LevelInfos& getBids() const {return bids_;}
-    const LevelInfos& getAsks() const {return asks_;}
+    // Adding some public APIs
+    const LevelInfos &getBids() const { return bids_; }
+    const LevelInfos &getAsks() const { return asks_; }
 
-    private:
+private:
     LevelInfos bids_;
     LevelInfos asks_;
 };
 
 class Order
 {
-    public:
-    Order(Ordertype orderType, OrderId orderId, Side side, Price price, Quatity quantity)
-    : orderType_{ orderType}
-    , orderId_{orderId}
-    , side_{side}
-    , price_{price}
-    , initialQuantity_{quantity}
-    , remainingQuantity{quantity}
+public:
+    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_{orderType}, orderId_{orderId}, side_{side}, price_{price}, initialQuantity_{quantity}, remainingQuantity_{quantity}
 
     { }
 
-    OrderType GetOrderType() const {return orderType_; }
-    OrderId GetOrderId() const {return orderId_; }
-    Side GetSide() const {return side_; }
-    Price GetPrice() const {return price_; }
-    Quantity GetInitialQuantity() const {return initialQuanity_; }
-    Quantity GetRemainingQuantity() const {return remainingQuantity_; }
-    
+    OrderType GetOrderType() const { return orderType_; }
+    OrderId GetOrderId() const { return orderId_; }
+    Side GetSide() const { return side_; }
+    Price GetPrice() const { return price_; }
+
+    Quantity GetInitialQuantity() const { return initialQuantity_; }
+    Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+    Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
+
+    void Fill(Quantity quantity)
+    {
+        if (quantity > GetRemainingQuantity())
+        {
+            throw logic_error(
+                format("Order ({}) cannot be filled for more than its remaining quantity.",
+                       GetOrderId()));
+        }
+
+        remainingQuantity_ -= quantity;
+    }
+
+private:
+    OrderType orderType_;
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity initialQuantity_;
+    Quantity remainingQuantity_;
 };
 
+using OrderPointer = shared_ptr<Order>;
+using OrderPointers = list<OrderPointer>;
+
+class OrderModify
+{
+public:
+    OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderId_{orderId}, side_{side}, price_{price}, quantity_{quantity}
+
+    { }
+
+    // writing the API calls
+    OrderId GetOrderId() const { return orderId_; }
+    Side GetSide() const { return side_; }
+    Price GetPrice() const { return price_; }
+    Quantity GetQuantity() const { return quantity_; }
+
+    OrderPointer ToOrderPointer(OrderType type) const
+    {
+        return make_shared<Order>(type, GetOrderId(), GetPrice(), GetSide(), GetQuantity());
+    }
+
+private:
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity quantity_;
+};
 int main()
 {
-    
+
     return 0;
 }
